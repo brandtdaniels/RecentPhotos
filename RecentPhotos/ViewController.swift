deed8a53cc9144bfc170f90deb759cddaf976745
Brandt Daniels <brandtdaniels@gmail.com>
2019-04-05 11:29:05 -0700

    Initial Commit

---
+++ RecentPhotos/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  RecentPhotos
+//
+//  Created by Brandt Daniels on 4/5/19.
+//  Copyright © 2019 Brandt Daniels. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
